ab70a45253f2e4ba19de6d0139bcf381b0aff37a
Carlos Carvalho <cccarv82@gmail.com>
2021-10-09 00:24:04 -0300

    update

--- home/.config/polybar/scripts/openweathermap-fullfeatured.sh
+++ home/.config/polybar/scripts/openweathermap-fullfeatured.sh
@@ -3,19 +3,19 @@
 get_icon() {
     case $1 in
         # Icons for Nerd Fonts
-        01d) icon="";;
-        01n) icon="";;
-        02d) icon="";;
-        02n) icon="";;
-        03d) icon="";;
-        03n) icon="";;
-        04*) icon="";;
-        09*) icon="";;
-        10d) icon="";;
-        10n) icon="";;
-        11*) icon="";;
-        13*) icon="";;
-        50*) icon="";;
+        01d) icon="";;
+        01n) icon="";;
+        02d) icon="";;
+        02n) icon="";;
+        03d) icon="";;
+        03n) icon="";;
+        04*) icon="";;
+        09*) icon="";;
+        10d) icon="";;
+        10n) icon="";;
+        11*) icon="";;
+        13*) icon="";;
+        50*) icon="";;
         *) icon="";
         #*) icon="";
     esac
@@ -37,7 +37,7 @@ get_duration() {
 KEY="f0060b47028a54500c466c7288e41d31"
 CITY="3469058"
 UNITS="metric"
-SYMBOL="°"
+SYMBOL="°C"
 SYMBOL2="%"
 
 API="https://api.openweathermap.org/data/2.5"
